78d19b28a8fd460274de16b47270329770d4b1b0
Ally Gerrie <alasdairwgerrie@gmail.com>
2017-04-27 09:18:59 +0100

    minor change to console file

--- db/console.rb
+++ db/console.rb
@@ -3,8 +3,8 @@ require_relative('../models/albums.rb')
 require_relative('../models/artists.rb')
 
 
-# Album.delete_all()
-# Artist.delete_all()
+Album.delete_all()
+Artist.delete_all()
 
 
 artist1 = Artist.new({
